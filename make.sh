ab09089a90939e50bd86ef6e06c466926c0355cf
Daniel <105582911+dnl-85@users.noreply.github.com>
2022-05-25 10:51:24 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -97,7 +97,8 @@ echo '... project is READY ! ...'
 # maintenant nous pouvons commencer
 # now we can work
 cd $1
-xfce4-terminal --command="notepadqq --new-window $1.c --new-window embedded/$1.h --new-window README.md"
+xfce4-terminal --command="nohup notepadqq --new-window $1.c --new-window embedded/$1.h --new-window README.md"
+rm nohup.out
 echo "... this terminal will clear screen in 10 seconds ..."
 sleep 10
 clear
